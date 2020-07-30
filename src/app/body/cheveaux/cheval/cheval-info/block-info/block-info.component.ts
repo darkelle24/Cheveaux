c5fdd563ef8088481a6b1ca09984029e80645fdc
Djilani CARDINEAU <djilani.cardineau@epitech.eu>
2020-07-30 17:37:30 +0200

    finish block + genealogie

--- src/app/body/cheveaux/cheval/cheval-info/block-info/block-info.component.ts
+++ src/app/body/cheveaux/cheval/cheval-info/block-info/block-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-block-info',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./block-info.component.scss']
 })
 export class BlockInfoComponent implements OnInit {
+
+  @Input() block: any;
 
   constructor() { }
 
--- src/app/body/cheveaux/cheval/cheval-info/carrousselle/carrousselle.component.ts
+++ src/app/body/cheveaux/cheval/cheval-info/carrousselle/carrousselle.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, Input } from '@angular/core';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-carrousselle',
   templateUrl: './carrousselle.component.html',
   styleUrls: ['./carrousselle.component.scss']
 })
-export class CarrousselleComponent implements OnInit {
 
-  constructor() { }
+export class CarrousselleComponent {
+  @Input() caroussel: any;
 
-  ngOnInit(): void {
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+
+  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
   }
 
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
 }
