70091cc7c6bb829035d4dbc0cbe8e3ca3a18b8ec
Djilani CARDINEAU <djilani.cardineau@epitech.eu>
2020-08-07 10:23:02 +0200

    add footer

--- src/app/header/nav/nav.component.ts
+++ src/app/header/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceAllService } from '../../services/service-all.service';
+import { Router, Event, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,9 @@ export class NavComponent implements OnInit {
 
   projets: any[];
 
-  constructor(private projetService: ServiceAllService) { }
+  toActive = 0
+
+  constructor(private projetService: ServiceAllService, private router: Router) { }
 
   ngOnInit() {
     this.projetService.getAllChevaux()
@@ -19,6 +22,24 @@ export class NavComponent implements OnInit {
           this.projets = data;
         },
         (err: any) => console.log('error :' + err)
-      );
+    );
+
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        if (event.url.includes('accueil') || event.url === '/') {
+          this.toActive = 0
+        } else if (event.url.includes('cheveaux')) {
+          this.toActive = 2
+        } else if (event.url.includes('info')) {
+          this.toActive = 3
+        } else if (event.url.includes('actualit%C3%A9')) {
+          this.toActive = 1
+        } else if (event.url.includes('contact')) {
+          this.toActive = 4
+        } else {
+          this.toActive = -1
+        }
+      }
+    });
   }
 }
